2234aa67392df7d976db3b497523a63e09533bd7
Andre Wilkinson <andre_w3@yahoo.com>
2025-01-03 15:12:28 -0500

    working on navbar

--- delva-studio/src/app/navBar.tsx
+++ delva-studio/src/app/navBar.tsx
@@ -1,1 +1,23 @@
 import React from "react";
+
+
+
+
+
+export default function NavBar() {
+  return (
+    <nav className="nav-bar">
+      <ul>
+        {/* <li>
+          <a href="#home"><h1>Delva Studio</h1></a>
+        </li> */}
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- delva-studio/src/app/page.tsx
+++ delva-studio/src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavBar from "./navBar";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
+      {/* <a href="#home"><h1>Delva Studio</h1></a> */}
+      <a href="#home">
         <h1 className="styles.title"> Delva Studio</h1>
+      </a>
+        <NavBar />
+      <main className={styles.main}>
         
         
 
